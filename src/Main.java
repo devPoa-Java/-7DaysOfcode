e4228d4be7798b943607a6925721a6d0cd02e420
Sandro M dos Santos <sandromarista@gmail.com>
2022-04-12 02:08:00 -0300

    Ajuste de codigo day 1

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,19 @@ import java.net.http.HttpResponse.BodyHandlers;
 public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-
+		
+		String apiKey = "APIKEY";
+		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_ckm07o4f"))
+				.uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKey))
 				.GET()
 				.build();
 
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		String json = response.body();
 
-		System.out.println(response.body());
+		System.out.println("Resposta:" + json);
 
 	}
 
